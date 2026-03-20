4135af33c0285816ec232fe95690b88fca198ba9
Seo ji-seop <seo941024@gmail.com>
2026-03-20 09:19:53 +0900

    Homework

--- Homework.py
+++ Homework.py
@@ -4,9 +4,49 @@ import random
 
 def random_num():
     return random.randint(1,100)
-target=random_num() #1,100까지의 숫자를 랜덤으로 추출하는 함수.
-                    #그 랜덤 값을 target이라는 변수라고 지정.
+target=random_num()
 print(target)
+
+window=tkinter.Tk()
+window.title("Up Down Game")
+window.geometry("640x480+0+0")
+window.resizable(True, True)
+
+tkinter.Label(window, text="1부터 100까지 중 숫자를 입력해 주세요!!!", width=500, relief="solid").pack()
+answer_entry = tkinter.Entry(window)
+answer_entry.pack()
+
+result_label=tkinter.Label(window, text="")
+result_label.pack()
+
+submit = tkinter.Button(window, text = "submit")
+submit.pack()
+
+restart = tkinter.Button(window, text = "Restart")
+restart.pack()
+
+while True:
+    n=int(entry.get(("1~100 number Enter : ")))
+    
+    if n==target :
+        label.config(text="정답인",text=target,text="입니다")
+
+        while True:
+            repeat = input("한번 더 하고싶다면 Y를, 종료하고 싶다면 N을 누르세요.")
+            if repeat in ['y','Y','n','N']:
+                break
+            else :
+                print("잘못된 입력입니다.")
+
+        if repeat in ['n','N']:
+            print("프로그램이 종료됩니다.")
+            break
+
+    elif n < target :
+        print("더 큰 숫자를 입력하세요.")
+
+    elif n > target : 
+        print("더 작은 숫자를 입력하세요.")
 
 #updown game
 '''정수를 입력.
@@ -16,16 +56,5 @@ print(target)
     정답이면 정답입니다! 와 함께, 한 번 더 할거면 Y, 아니면 N 입력
     Y 누를시 다시 1로 돌아감. 아니면 break.    '''
 #지금 해야할 것 > 이걸 함수로 정의한다.
-
-window=tkinter.Tk()
-window.title("Up Down Game")
-window.geometry("640x480+0+0")
-window.resizable(True, True)
-
-tkinter.Label(window, text="1부터 100까지 중 숫자를 입력해 주세요!!!",\
-              width=500, relief="solid").pack()
-answer_entry = tkinter.Entry(window)
-answer_entry.pack()
-
 
 window.mainloop()
--- Homework_2.py
+++ Homework_2.py
@@ -3,27 +3,33 @@ import random
 def random_num():
     return random.randint(1,100)
 target=random_num()
-print(target)
 
 while True:
-    n=int(input("1~100 number Enter : "))
+    target=random_num()
     
-    if n==target :
-        print("정답인",target,"입니다")
+    while True:
+        n=int(input("1~100 number Enter : "))
+        
+        if n==target :
+            print("정답인",target,"입니다")
 
-        while True:
-            repeat = input("한번 더 하고싶다면 Y를, 종료하고 싶다면 N을 누르세요.")
-            if repeat in ['y','Y','n','N']:
+            while True:
+                repeat = input("한번 더 하고싶다면 Y를, 종료하고 싶다면 N을 누르세요.")
+                if repeat in ['y','Y','n','N']:
+                    break
+                else :
+                    print("잘못된 입력입니다.")
+
+            if repeat in ['n','N']:
+                print("프로그램이 종료됩니다.")
                 break
             else :
-                print("잘못된 입력입니다.")
+                target=random_num()
+                
+        elif n < target :
+            print("더 큰 숫자를 입력하세요.")
 
-        if repeat in ['n','N']:
-            print("프로그램이 종료됩니다.")
-            break
+        elif n > target : 
+            print("더 작은 숫자를 입력하세요.")
 
-    elif n < target :
-        print("더 큰 숫자를 입력하세요.")
-
-    elif n > target : 
-        print("더 작은 숫자를 입력하세요.")
+#up down 게임 구동장치
